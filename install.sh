89d92c4a7a44f3a0774dc9df7cd65f1e29fa6192
PeterC <petercalifano.gs@gmail.com>
2024-12-10 22:50:05 +0100

    Added install script and WS config

---
+++ install.sh
@@ -0,0 +1,17 @@
+# Update submodules
+git submodules update --init --recursive
+
+# Install dependencies
+sudo apt install libgflags-dev libgoogle-glog-dev
+
+# Install opencv
+sudo apt install libopencv-dev
+
+# Install others required by build (not reported by authors)
+sudo apt install libunwind-dev
+
+# Make build 
+cmake -B build -S . -DCMAKE_BUILD_TYPE=relwithdebinfo -DGUI=true
+
+# Make
+make -C build -j4
